1b6298957ffbe30dc8c9a2cdf93fcae0ba99b5a5
yamotonalds <yamotonalds@gmail.com>
2022-10-28 22:59:32 +0900

    Config deploy

--- deploy.sh
+++ deploy.sh
@@ -15,12 +15,13 @@ cd dist
 git init
 git checkout -B main
 git add -A
-git commit -m 'deploy'
+git commit -m 'Deploy'
 
 # https://<USERNAME>.github.io にデプロイする場合
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # https://<USERNAME>.github.io/<REPO> にデプロイする場合
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f https://github.com/yamotonalds/vtuber-schedule.git main:gh-pages
 
 cd -
