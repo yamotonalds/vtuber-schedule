a3350e6530cd30214f97555b1f368b02137d6e87
yamotonalds <yamotonalds@gmail.com>
2022-10-29 01:41:09 +0900

    Make it possible to get text of input image

--- src/components/VtuberSchedule.vue
+++ src/components/VtuberSchedule.vue
@@ -1,20 +1,27 @@
 <script setup>
 import { ref } from 'vue'
+import { createWorker } from 'tesseract.js';
 
 const image_type = ref('')
 const image_data_url = ref('')
+const text_in_image = ref('')
 
-async function on_paste(e) {
+function on_paste(e) {
   if (e.clipboardData.types.length == 0) return
 
   const item = e.clipboardData.items[0]
   image_type.value = item.type
   if ( item.type.startsWith('image/')) {
     const file = item.getAsFile()
-    var reader = new FileReader()
-    reader.onload = (e) => {
+    const reader = new FileReader()
+    reader.onload = async (e) => {
       const data_url = e.target.result
       image_data_url.value = data_url
+      text_in_image.value = '読込中'
+
+      const text = await ocr(data_url)
+      console.log(text)
+      text_in_image.value = text
     }
     reader.readAsDataURL(file)
   } else {
@@ -29,15 +36,33 @@ function on_file_drop(e) {
   image_type.value = file.type
 
   if (file.type.startsWith('image/')) {
-    var reader = new FileReader()
-    reader.onload = (e) => {
+    const reader = new FileReader()
+    reader.onload = async (e) => {
       const data_url = e.target.result
       image_data_url.value = data_url
+      text_in_image.value = '読込中'
+
+      const text = await ocr(data_url)
+      console.log(text)
+      text_in_image.value = text
     };
     reader.readAsDataURL(file)
   } else {
     alert('対応していないファイルです。')
   }
+}
+
+async function ocr(image) {
+  const ocr_worker = createWorker({
+    logger: m => console.log(m)
+  })
+  await ocr_worker.load()
+  await ocr_worker.loadLanguage('jpn+eng')
+  await ocr_worker.initialize('jpn+eng')
+  const { data: { text } } = await ocr_worker.recognize(image)
+  await ocr_worker.terminate()
+
+  return text
 }
 </script>
 
@@ -45,6 +70,7 @@ function on_file_drop(e) {
   <textarea class="source-input" placeholder="ここに画像を貼り付けるか、画像ファイルをドラッグ＆ドロップしてください。" v-on:paste="on_paste" v-on:drop.prevent="on_file_drop" v-on:dragover.prevent readonly />
   <p>画像の種類: {{ image_type }}</p>
   <img class="input-image" :src="image_data_url" />
+  <div class="text-in-image">{{ text_in_image }}</div>
 </template>
 
 <style scoped>
@@ -56,5 +82,9 @@ function on_file_drop(e) {
 
 .input-image {
   width: 500px;
+}
+
+.text-in-image {
+  white-space: pre-wrap;
 }
 </style>
